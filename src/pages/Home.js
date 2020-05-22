a0e93256d0386cbb3fd12077f326d8f805a42ab9
jordelucas <jordevalucas@hotmail.com>
2020-05-21 22:17:00 -0300

    create page folder

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledTitle = styled.h1`
+    color: #fff;
+    text-align: center;
+`
+
+export default function Home() {
+    return (
+        <StyledTitle>Hi. My name is Jordevá</StyledTitle>
+    )
+}
