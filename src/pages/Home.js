c38b41a66b5bf8a9378dde48722a2ba564464d4e
jordelucas <jordevalucas@hotmail.com>
2020-05-26 20:24:46 -0300

    change styled-component of the home screen

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,46 +6,14 @@ import styled, { css } from 'styled-components'
 const StyledContainer = styled.main`
     flex: 1;
     display: none;
-    justify-content: center;
-    align-items: center;
-    color: #fff;
+    margin: 0 20px;
 
-    div {
-        max-width: 500px;
-        max-height: 500px;
-        padding: 0 10px;
-
-        h1 {
-            font-weight: normal;
-            margin: 10px 0 35px 0;
-            font-size: 3rem;
-        }
-
-        span {
-            color: #808080;
-            font-weight: bold;
-            text-transform: uppercase;
-        }
-
-        img {
-            width: 100%;
-            max-width: 400px;
-        }
-
-        button {
-            padding: 10px 30px;
-            border: 2px solid #fff;
-            background-color: transparent;
-            color: #fff;
-            cursor: pointer;
-            text-transform: uppercase;
-            transition: 0.5s;
-
-            &:hover {
-                background-color: #fff;
-                color: #090909;
-            }
-        }
+    section {
+        display: grid;
+        grid-template-columns: 2fr 1.5fr;
+        gap: 20px;
+        width: 768px;
+        margin: auto;
     }
 
     ${({ active }) =>
@@ -54,20 +22,59 @@ const StyledContainer = styled.main`
         `}
 `
 
+const StyledImage = styled.aside`
+    img {
+        width: 100%;
+        max-width: 400px;
+    }
+`
+
+const StyledText = styled.div`
+    h1 {
+        color: #fff;
+        font-size: 3rem;
+        font-weight: normal;
+        margin: 10px 0 35px 0;
+    }
+
+    span {
+        color: #808080;
+        font-weight: bold;
+        text-transform: uppercase;
+    }
+
+    button {
+        padding: 10px 30px;
+        border: 2px solid #fff;
+        background-color: transparent;
+        color: #fff;
+        cursor: pointer;
+        text-transform: uppercase;
+        transition: 0.5s;
+
+        &:hover {
+            background-color: #fff;
+            color: #090909;
+        }
+    }
+`
+
 export default function Home(props) {
     return (
         <>
         <StyledContainer active={props.active}>
-            <div>
-                <span>&lt;Hello World&gt;</span>
-                <h1>Hi. My name is Jordevá Lucas.
-                    I'm junior frontend developer from Natal.</h1>
-                <button>Contact me</button>
-            </div>
-            
-            <div>
-                <img src={profile} alt="profile" />
-            </div>
+            <section>
+                <StyledText>
+                    <span>&lt;Hello World&gt;</span>
+                    <h1>Hi. My name is Jordevá Lucas.
+                        I'm junior frontend developer from Natal.</h1>
+                    <button>Contact me</button>
+                </StyledText>
+                
+                <StyledImage>
+                    <img src={profile} alt="profile" />
+                </StyledImage>
+            </section>
         </StyledContainer>
         </>
     )
