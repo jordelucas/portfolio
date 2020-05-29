24604658e369ea965db625a9f6f8d8717cc87d51
jordelucas <jordevalucas@hotmail.com>
2020-05-28 21:18:08 -0300

    add hyperlinks in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,22 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledFooter = styled.footer`
-    color: #808080;
     text-align: center;
     padding: 20px 0;
-    text-transform: uppercase;
-    font-weight: bold;
     
     ul {
-        li {
-            padding: 0 10px;
+        a {
             display: inline;
+            padding: 0 10px;
+            color: #808080;
+            font-weight: bold;
+            text-transform: uppercase;
+            text-decoration: none;
+            transition-duration: 0.3s;
+
+            &:hover {
+                color: #ccc;
+            }
         }
     }
 `
@@ -20,9 +26,9 @@ function Footer() {
     return (
         <StyledFooter>
             <ul>
-                <li>GitHub</li>
-                <li>Instagram</li>
-                <li>Linkedin</li>
+                <a href='https://github.com/jordelucas/' target="_blank">GitHub</a>
+                <a href='https://www.instagram.com/jordevalucas/' target="_blank">Instagram</a>
+                <a href='https://www.linkedin.com/in/jordelucas' target="_blank">Linkedin</a>
             </ul>
         </StyledFooter>  
     )
